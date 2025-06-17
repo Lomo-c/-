5cf244f4c795b7b5ae4113887b280a4328c2d248
Lomo-c <asil2004@163.com>
2025-06-17 21:30:07 +0800

    111

---
+++ Bilibili-java/src/main/java/com/jishu/BilibiliJavaApplication.java
@@ -0,0 +1,13 @@
+package com.jishu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilibiliJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilibiliJavaApplication.class, args);
+    }
+
+}
