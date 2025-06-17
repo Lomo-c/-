151d596dec1d5c5e75d34be08ee0fb3fc16ae204
XIAOJIAN680 <1423317514@qq.com>
2025-06-17 21:42:31 +0800

    。

---
+++ Bilibili-java/bilibili-user/src/main/java/com/jishu/service/IMenuitemsService.java
@@ -0,0 +1,7 @@
+package com.jishu.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.jishu.domain.Menultems;
+
+public interface IMenuitemsService extends IService<Menultems> {
+} 
